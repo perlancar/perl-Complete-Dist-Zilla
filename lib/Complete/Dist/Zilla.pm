91d249f9b1aff42dce31e71d08e82647a3973b98
perlancar (@pc-home) <perlancar@gmail.com>
2014-12-28 18:33:07 +0700

    Oops, forgot to add list_dzil_bundle & list_dzil_role

--- lib/Complete/Dist/Zilla.pm
+++ lib/Complete/Dist/Zilla.pm
@@ -19,10 +19,7 @@ our @EXPORT_OK = qw(
                        complete_dzil_role
                );
 
-$SPEC{complete_dzil_plugin} = {
-    v => 1.1,
-    summary => 'Complete with installed Dist::Zilla plugin names',
-    args => {
+my %common_args = (
         word => {
             schema => 'str*',
             req => 1,
@@ -38,6 +35,13 @@ $SPEC{complete_dzil_plugin} = {
         exp_im_path => {
             schema => 'bool',
         },
+);
+
+$SPEC{complete_dzil_plugin} = {
+    v => 1.1,
+    summary => 'Complete with installed Dist::Zilla plugin names',
+    args => {
+        %common_args,
     },
     result_naked => 1,
 };
@@ -54,6 +58,56 @@ sub complete_dzil_plugin {
     Complete::Module::complete_module(
         word => $word,
         ns_prefix => 'Dist::Zilla::Plugin',
+        ci=>$ci, map_case=>$map_case, exp_im_path=>$exp_im_path,
+    );
+}
+
+$SPEC{complete_dzil_role} = {
+    v => 1.1,
+    summary => 'Complete with installed Dist::Zilla role names',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub complete_dzil_role {
+    require Complete::Module;
+
+    my %args = @_;
+
+    my $word = $args{word} // '';
+    my $ci          = $args{ci} // $Complete::OPT_CI;
+    my $map_case    = $args{map_case} // $Complete::OPT_MAP_CASE;
+    my $exp_im_path = $args{exp_im_path} // $Complete::OPT_EXP_IM_PATH;
+
+    Complete::Module::complete_module(
+        word => $word,
+        ns_prefix => 'Dist::Zilla::Role',
+        ci=>$ci, map_case=>$map_case, exp_im_path=>$exp_im_path,
+    );
+}
+
+$SPEC{complete_dzil_bundle} = {
+    v => 1.1,
+    summary => 'Complete with installed Dist::Zilla bundle names',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub complete_dzil_bundle {
+    require Complete::Module;
+
+    my %args = @_;
+
+    my $word = $args{word} // '';
+    my $ci          = $args{ci} // $Complete::OPT_CI;
+    my $map_case    = $args{map_case} // $Complete::OPT_MAP_CASE;
+    my $exp_im_path = $args{exp_im_path} // $Complete::OPT_EXP_IM_PATH;
+
+    Complete::Module::complete_module(
+        word => $word,
+        ns_prefix => 'Dist::Zilla::PluginBundle',
         ci=>$ci, map_case=>$map_case, exp_im_path=>$exp_im_path,
     );
 }
